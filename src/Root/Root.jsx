b0ee444b53ebc6d7844c8c01a186f5f32349f257
omarjafor <omarjaforchy@gmail.com>
2023-10-06 15:20:26 +0600

    some component added

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
+import { Toaster } from 'react-hot-toast'
 
 const Root = () => {
     return (
         <div className=""> 
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
+            <Toaster />
         </div>
     );
 };
